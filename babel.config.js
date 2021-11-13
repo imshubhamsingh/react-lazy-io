581483f1175c50c97d422874c14d208f8e34af43
Shubham Singh <imshubhamsingh97@gmail.com>
2021-11-13 12:56:30 +0530

    chore: initial commit

---
+++ babel.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic',
+      },
+    ],
+    ['@babel/preset-typescript'],
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage',
+        corejs: 3,
+        loose: true,
+        shippedProposals: true,
+        targets: {
+          browsers: [
+            '>5%',
+            'last 2 Chrome versions',
+            'last 2 Edge versions',
+            'last 2 Firefox versions',
+            'last 2 Safari versions',
+          ],
+        },
+      },
+    ],
+  ],
+  plugins: [
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+    '@babel/plugin-transform-runtime',
+    'babel-plugin-styled-components',
+  ],
+};
