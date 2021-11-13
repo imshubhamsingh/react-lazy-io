9ea0ba1e0b786e0da8ff01ef8914309ecf86573f
Shubham Singh <imshubhamsingh97@gmail.com>
2021-11-14 00:41:50 +0530

    chore: added test projects to test

--- src/Lazy.Placeholder.tsx
+++ src/Lazy.Placeholder.tsx
@@ -1,6 +1,13 @@
-function LazyPlaceHolder({ as, children, ...props }) {
-  const Tag = as || 'div';
-  return <Tag>{children}</Tag>;
+import * as React from 'react';
+interface LazyPlaceholderProps {
+  as: keyof JSX.IntrinsicElements;
 }
+
+const LazyPlaceHolder: React.FunctionComponent<
+  LazyPlaceholderProps & React.HTMLAttributes<HTMLOrSVGElement>
+> = ({ as, children, ...props }) => {
+  const Tag = as || 'div';
+  return <Tag {...props}>{children}</Tag>;
+};
 
 export default LazyPlaceHolder;
